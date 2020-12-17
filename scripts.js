d11413061c32e5ea30225c80f566394b2b0100bf
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2020-12-17 16:05:24 -0600

    v2 (Currently has rainbow trail capability)
    need to work on changing grid through prompt() capabilities

--- scripts.js
+++ scripts.js
@@ -1,25 +1,75 @@
-/* need an onload() function i believe here, or just call right away at minimum */
 
 window.onload = function() { 
     startingGrid(4);
-    let validCell = document.getElementById("cell");
-    /* WHY can this not see #cell */
-    let gridList = document.querySelectorAll(".cell");
-    let count = 0;
-    /*console.log(gridList.length);*/
-    for (count = 0; count < gridList.length; count++) {
-        gridList[count].addEventListener("mouseover", leaveTrail);
-        gridList[count].addEventListener("mouseout", clearTrail);
-    }
+    document.getElementById("main-grid-contain").style.gridTemplateColumns = "repeat(4, 100px)";
+    let randomized = false;
+    
+    
+    $("#change-grid-size") 
+        .click(function() {
+            let newSize = parseInt(prompt("Please enter desired number of cells"));
+            startingGrid(newSize);
+            document.getElementById("main-grid-contain").style.gridTemplateColumns = `repeat(${newSize} , 100px)`;
+            /* have to properly decrease size of grid if # of rows/cols is < current vals */
+            /* aka need to remove (?) the current ones or just add/subtract what is needed to current grid */
+    })
+    
+    $("#randomize-colors")
+        .click(function() {
+        if (!randomized) {
+            randomized = true;
+            console.log(randomized);
+        }
+        else {
+            randomized = false;
+            console.log(randomized);
+        }
+    });
+    
+    $("#main-grid-contain div")
+        /*if (randomized === true) {
+            .mouseover(function() {
+                $(this).style.backgroundColor = "#444444";
+            });
+            .mouseout(function() {
+                $(this).style.backgroundColor = "#FFFFFF";
+            });
+        }
+        */
+            .mouseover(function() {
+                if (randomized) {
+                    $(this).css("background-color", `rgb(${applyRandomColor()}, ${applyRandomColor()}, ${applyRandomColor()})`);
+                    console.log("hit");
+                }
+                else {
+                    $(this).css("background-color", "");
+                    $(this).toggleClass("darkcell");   
+                }
+            })
+    
+            .mouseout(function() {
+                if (randomized) {
+                    $(this).css("background-color", "#FFFFFF");
+                }
+                else {
+                        $(this).css("background-color", "");
+                        $(this).toggleClass("darkcell");
+                }
+            
+            })
+        
 }
 
-/*document.body.onload = startingGrid*/
+function applyRandomColor() {
+    return Math.floor(Math.random(0, 1) * 255);
+}
+
 
 
 
 function startingGrid(num) {
     let idcounter = 1;
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < num ** 2; i++) {
         let gridCell = document.createElement("div");
         /*let innertext = document.createTextNode("Cell");*/
         /*gridCell.appendChild(innertext);*/
@@ -27,79 +77,13 @@ function startingGrid(num) {
             idcounter++;
         }
         gridCell.setAttribute("id", "cell" + idcounter.toString());
+        /*gridCell.setAttribute("class", "dynamCell");*/
 
         document.getElementById("main-grid-contain").appendChild(gridCell);
-        for (let j = 0; j < 3; j++) {
-            let gridCell = document.createElement("div");
-            /*let test = document.createTextNode("Cell 2");*/
-            /*gridCell.appendChild(test);*/
-            idcounter++;
-            gridCell.setAttribute("id", "cell" + idcounter.toString());
-            document.getElementById("main-grid-contain").appendChild(gridCell);
-        }   
+        /*document.getElementById("cell" + idcounter.toString()).innerHTML = "Cell";*/
+        
     }   
-    /*console.log(gridCell);*/
+    
 }
     
-/*
-let gridList = document.querySelectorAll("cell");
-
-let validCell = document.getElementById("cell");
-
-if (gridList) {
-    gridList[4].addEventListener("onmouseover", leaveTrail);
-    gridList[4].addEventListener("onmouseout", clearTrail);
-}
-*/
-
-/* how to access/properly remove the id while still being able to see cells */
-/* use a queryselectorall to target each individual cell, then */
-/* then when you target the divs again you can just skip the first major div */
-
-
-
-function leaveTrail() {
-    /* how would you pinpoint which cell is being hovered/should be targeted */
-    /* ^^^^^^ */
-    /* ultimately, you would want each cell to have a unique class/id to be able to target a specific one */
-    /*validCell.removeAttribute("id");*/
-    
-    
-    /* need to make this be able to accept arbitrary number of "#cell[number]" */
-    /* BIGGG make them all have same class, then target div .[class] and have that be in place of #cell currently */
-    let gridList = document.querySelectorAll("#cell");
-    let leaveCount = 0;
-    console.log(gridList.length);
-    for (leaveCount = 1; leaveCount < gridList.length+1; leaveCount++) {
-        console.log(leaveCount);
-        console.log("shows up!");
-        if (document.getElementById("cell" + leaveCount.toString())[leaveCount]) {
-            document.getElementById("cell" + leaveCount.toString())[leaveCount].style["background-color"] = "#444";
-            }
-    }
-}
-
-function clearTrail() {
-    let gridList = document.querySelectorAll(".darkcell");
-    let clearCount = 0;
-    for (clearCount = 1; clearCount < gridList.length+1; clearCount++) { 
-        if (document.getElementById("darkcell" + clearCount.toString())[clearCount]) {
-         document.getElementById("darkcell" + clearCount.toString())[clearCount].style["background-color"] = "#FFFFFF"; 
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
