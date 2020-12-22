51da97158bcff76b3b1466ed92f8b07e5dda0d05
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2020-12-22 07:19:49 -0600

    fixed everything but responsive div height
    nice

--- scripts.js
+++ scripts.js
@@ -1,57 +1,148 @@
-/* need an onload() function i believe here, or just call right away at minimum */
+let randomized = false;
+let showTrail = true;
+let timesPassedOver = 0;
+let cellArr;
+let newSize;
+let currentSelectedID;
+let isntInitialGrid = false;
+let updatedCellWidth = 240;
 
-document.body.onload = startingGrid;
+
+window.onload = function() { 
+    startingGrid(4);
+    document.getElementById("main-grid-contain").style.gridTemplateColumns = `repeat(4, ${updatedCellWidth.toString()}px)`;
+    console.log(`${updatedCellWidth.toString()}px`);     
+    newSize = 16;
+    cellArr = new Array(newSize).fill(0);
+    
+    /* going to need to figure out how to select ALL divs (aka use #main-grid-contain > div) here to 
+        scale heights as well. maybe need jquery here too? */
+    
+    
+    $("#change-grid-size") 
+        .click(function() {
+            resetGrid();
+            newSize = parseInt(prompt("Please enter desired number of cells", "4"));
+            isntInitialGrid = true;
+            startingGrid(newSize);
+            cellArr = new Array(newSize ** 2).fill(0);
+            document.getElementById("main-grid-contain").style.gridTemplateColumns = `repeat(${newSize} , ${(updatedCellWidth/newSize).toString()}px)`;
+    })
+    
+    $("#randomize-colors")
+        .click(function() {
+        if (!randomized) {
+            randomized = true;
+            console.log(randomized);
+        }
+        else {
+            randomized = false;
+            console.log(randomized);
+        }
+        cellArr = new Array(newSize ** 2).fill(0);
+    });
+    
+    $("#trail-toggle")
+        .click(function() {
+        if (!showTrail) {
+            showTrail = true;
+            console.log(showTrail);
+        }
+        else {
+            showTrail = false;
+            console.log(showTrail);
+        }
+        cellArr = new Array(newSize ** 2).fill(0);
+    })
+    
+    $("#main-grid-contain div")
+    
+            .mouseover(function() {
+                    currentSelectedID = $(this).attr("id").slice(4);
+                    currentSelectedID = parseInt(currentSelectedID) - 1;
+                    if (cellArr[currentSelectedID] < 10) {
+                        cellArr[currentSelectedID]++;
+                        console.log(cellArr);
+                        console.log(currentSelectedID);
+                    }
+                    else {
+
+                    }
+                    
+                    if (randomized) {
+                        $(this).css("background-color", `rgba(${applyRandomColor()}, ${applyRandomColor()}, ${applyRandomColor()}, ${cellArr[currentSelectedID] * 0.1})`);
+                        console.log("hit");
+                    }
+                    else {
+                        $(this).css("background-color", `rgba(0, 0, 0, ${cellArr[currentSelectedID] * 0.1})`);
+                        /*$(this).toggleClass("darkcell");*/   
+                    }
+                
+            })
+    
+            
+            .mouseout(function() {
+                if (!showTrail) {
+                    $(this).css("background-color", "#FFFFFF");
+                }
+            })
+    
+            
+    
+}
+
+
+function applyRandomColor() {
+    return Math.floor(Math.random(0, 1) * 255);
+}
+
+
+function resetGrid() {
+    $("#main-grid-contain").children().remove();
+    
+}
 
 function startingGrid(num) {
-    for (let i = 0; i < 4; i++) {
+    let idcounter = 1;
+    for (let i = 0; i < num ** 2; i++) {
         let gridCell = document.createElement("div");
-        let test = document.createTextNode("Cell");
-        gridCell.appendChild(test);
-        gridCell.setAttribute("id", "cell");
+        if (i !== 0) {
+            idcounter++;
+        }
+        gridCell.setAttribute("id", "cell" + idcounter.toString());
 
         document.getElementById("main-grid-contain").appendChild(gridCell);
-        for (let j = 0; j < 3; j++) {
-            let gridCell = document.createElement("div");
-            let test = document.createTextNode("Cell 2");
-            gridCell.appendChild(test);
-            gridCell.setAttribute("id", "cell");
-            document.getElementById("main-grid-contain").appendChild(gridCell);
-        }   
-    }   
+    }
+    
+    if (isntInitialGrid) {
+    
+         $("#main-grid-contain div")
+                .mouseover(function() {
+                        let currentSelectedID = $(this).attr("id").slice(4);
+                        currentSelectedID = parseInt(currentSelectedID) - 1;
+                        if (cellArr[currentSelectedID] < 10) {
+                            cellArr[currentSelectedID]++;
+                        }
+                        else {
+
+                        }
+
+                        if (randomized) {
+                            $(this).css("background-color", `rgba(${applyRandomColor()}, ${applyRandomColor()}, ${applyRandomColor()}, ${cellArr[currentSelectedID] * 0.1})`);
+                            console.log("hit");
+                        }
+                        else {
+                            $(this).css("background-color", `rgba(0, 0, 0, ${cellArr[currentSelectedID] * 0.1})`);
+                            /*$(this).toggleClass("darkcell");*/   
+                        }
+
+                })
+
+                .mouseout(function() {
+                    if (!showTrail) {
+                        $(this).css("background-color", "#FFFFFF");
+                    }
+                })
+    }
 }
-
-let validCell = document.getElementById("cell");
-
-if (validCell) {
-    validCell.addEventListener("onmouseenter", leaveTrail);
-    validCell.addEventListener("onmouseout", leaveTrail);
-}
-
-/* how to access/properly remove the id while still being able to see cells */
-/* use a queryselectorall to target each individual cell, then */
-/* then when you target the divs again you can just skip the first major div */
-
-function leaveTrail() {
-    validCell.removeAttribute("id");
-    document.getElementById("cell").setAttribute("id", "darkcell");
-   
-}
-
-function clearTrail() {
-    document.getElementById("cell").setAttribute("id", "cell");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
