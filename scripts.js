abf311c4feb521bbe97556f498c35e036b5d90ca
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2020-12-22 08:12:59 -0600

    need vert. padding

--- scripts.js
+++ scripts.js
@@ -1,24 +1,32 @@
 let randomized = false;
 let showTrail = true;
 let timesPassedOver = 0;
-let cellArr = [];
+let cellArr;
+let newSize;
+let currentSelectedID;
+let isntInitialGrid = false;
+let updatedCellWidth = 240;
+
+
 window.onload = function() { 
     startingGrid(4);
-    document.getElementById("main-grid-contain").style.gridTemplateColumns = "repeat(4, 100px)";
-    /* make 16 len arr here too */
-    /* cellArr = */
-    /* want to initialize with 0's first so that you can iterate over it (check on this logic) */
+    document.getElementById("main-grid-contain").style.gridTemplateColumns = `repeat(4, ${updatedCellWidth.toString()}px)`;
+    console.log(`${updatedCellWidth.toString()}px`);     
+    newSize = 16;
+    cellArr = new Array(newSize).fill(0);
+    
+    /* going to need to figure out how to select ALL divs (aka use #main-grid-contain > div) here to 
+        scale heights as well. maybe need jquery here too? */
+    
     
     $("#change-grid-size") 
         .click(function() {
-            let newSize = parseInt(prompt("Please enter desired number of cells"));
             resetGrid();
+            newSize = parseInt(prompt("Please enter desired number of cells", "4"));
+            isntInitialGrid = true;
             startingGrid(newSize);
-            /* create array of newSize */
-            document.getElementById("main-grid-contain").style.gridTemplateColumns = `repeat(${newSize} , 100px)`;
-            /* document.getElementById("main-grid-contain").style.backgroundColor = "red"; */
-            /* probably want to scale the size of the cells based on the number returned */
-            timesPassedOver = 0;
+            cellArr = new Array(newSize ** 2).fill(0);
+            document.getElementById("main-grid-contain").style.gridTemplateColumns = `repeat(${newSize} , ${(updatedCellWidth/newSize).toString()}px)`;
     })
     
     $("#randomize-colors")
@@ -31,7 +39,7 @@ window.onload = function() {
             randomized = false;
             console.log(randomized);
         }
-        timesPassedOver = 0;
+        cellArr = new Array(newSize ** 2).fill(0);
     });
     
     $("#trail-toggle")
@@ -44,37 +52,32 @@ window.onload = function() {
             showTrail = false;
             console.log(showTrail);
         }
-        timesPassedOver = 0;
+        cellArr = new Array(newSize ** 2).fill(0);
     })
     
     $("#main-grid-contain div")
-        /*if (randomized === true) {
+    
             .mouseover(function() {
-                $(this).style.backgroundColor = "#444444";
-            });
-            .mouseout(function() {
-                $(this).style.backgroundColor = "#FFFFFF";
-            });
-        }
-        */
-            .mouseover(function() {
-                /* need to make this PER CELL also need to CHANGE THIS */
-                if (timesPassedOver < 10) {
-                    timesPassedOver++;
-                    
-                    /* use this + .slice....but you need to stop at first 'l' */
-                        let currentSelectedID = $(this).attr("id");
-                    /*for (let q = len()) */
+                    currentSelectedID = $(this).attr("id").slice(4);
+                    currentSelectedID = parseInt(currentSelectedID) - 1;
+                    if (cellArr[currentSelectedID] < 10) {
+                        cellArr[currentSelectedID]++;
+                        console.log(cellArr);
+                        console.log(currentSelectedID);
+                    }
+                    else {
+
+                    }
                     
                     if (randomized) {
-                        $(this).css("background-color", `rgb(${applyRandomColor()}, ${applyRandomColor()}, ${applyRandomColor()},                           ${timesPassedOver * 0.1})`);
+                        $(this).css("background-color", `rgba(${applyRandomColor()}, ${applyRandomColor()}, ${applyRandomColor()}, ${cellArr[currentSelectedID] * 0.1})`);
                         console.log("hit");
                     }
                     else {
-                        $(this).css("background-color", `rgb(255,255,255, ${timesPassedOver * 0.1})`);
+                        $(this).css("background-color", `rgba(0, 0, 0, ${cellArr[currentSelectedID] * 0.1})`);
                         /*$(this).toggleClass("darkcell");*/   
                     }
-                }
+                
             })
     
             
@@ -87,6 +90,7 @@ window.onload = function() {
             
     
 }
+
 
 function applyRandomColor() {
     return Math.floor(Math.random(0, 1) * 255);
@@ -102,45 +106,43 @@ function startingGrid(num) {
     let idcounter = 1;
     for (let i = 0; i < num ** 2; i++) {
         let gridCell = document.createElement("div");
-        /*let innertext = document.createTextNode("Cell");*/
-        /*gridCell.appendChild(innertext);*/
         if (i !== 0) {
             idcounter++;
         }
         gridCell.setAttribute("id", "cell" + idcounter.toString());
-        /*gridCell.setAttribute("class", "dynamCell");*/
 
         document.getElementById("main-grid-contain").appendChild(gridCell);
-        /*document.getElementById("cell" + idcounter.toString()).innerHTML = "Cell";*/
-        
-        
-        
-        
-        
     }
     
-     $("#main-grid-contain div")
-            .mouseover(function() {
-                if (timesPassedOver < 10) {
-                    timesPassedOver++;
-                    
-                    if (randomized) {
-                        $(this).css("background-color", `rgb(${applyRandomColor()}, ${applyRandomColor()}, ${applyRandomColor()},                           ${timesPassedOver * 0.1})`);
-                        console.log("hit");
-                    }
-                    else {
-                        $(this).css("background-color", `rgb(255,255,255, ${timesPassedOver * 0.1})`);
-                        /*$(this).toggleClass("darkcell");*/   
-                    }
-                }
-            })
+    if (isntInitialGrid) {
     
-            .mouseout(function() {
-                if (!showTrail) {
-                    $(this).css("background-color", "#FFFFFF");
-                }
-            })
-    
+         $("#main-grid-contain div")
+                .mouseover(function() {
+                        let currentSelectedID = $(this).attr("id").slice(4);
+                        currentSelectedID = parseInt(currentSelectedID) - 1;
+                        if (cellArr[currentSelectedID] < 10) {
+                            cellArr[currentSelectedID]++;
+                        }
+                        else {
+
+                        }
+
+                        if (randomized) {
+                            $(this).css("background-color", `rgba(${applyRandomColor()}, ${applyRandomColor()}, ${applyRandomColor()}, ${cellArr[currentSelectedID] * 0.1})`);
+                            console.log("hit");
+                        }
+                        else {
+                            $(this).css("background-color", `rgba(0, 0, 0, ${cellArr[currentSelectedID] * 0.1})`);
+                            /*$(this).toggleClass("darkcell");*/   
+                        }
+
+                })
+
+                .mouseout(function() {
+                    if (!showTrail) {
+                        $(this).css("background-color", "#FFFFFF");
+                    }
+                })
+    }
 }
     
-
